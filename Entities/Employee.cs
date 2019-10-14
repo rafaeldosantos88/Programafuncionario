5b6d5bea0547020e2d1378f754934e5def60c7d1
Rafael Dos Santos <Rafael dos Santos>
2019-10-14 15:08:24 -0300

    Criada a classe Employee(Funcionario

---
+++ Entities/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course.Entities
+{
+    class Employee //Classe Funcionario                       
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; } //Horas trabalhada pelo funcionario
+        public double ValuePerHour{ get; set; } //Valor de cada hora trabalhada
+
+        public Employee()                     //Construtor padrão
+        {
+
+        }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+        public double  Payment()               //Operação pagamento
+        {
+            return Hours * ValuePerHour;      // A operação pagamento  vai ser "Horas" * vezes "valor por hora"
+
+        }
+    }
+}
